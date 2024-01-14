5d9fe6e7aca4f8b8fb1d6e97dd0fd3b1c0ea2ad0
yk1979 <yukiirisawa1979@gmail.com>
2024-01-14 10:47:22 +0900

    fix layout and add error component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
+import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1 className={styles.heading}>
+          <Link href="/">Root Layout</Link>
+        </h1>
+        {children}
+      </body>
     </html>
   );
 }
