00bf03edecfb286f59b179596c133c14597cb752
yk1979 <yukiirisawa1979@gmail.com>
2024-01-14 14:09:15 +0900

    fix typo: donuts -> donut

--- src/app/_components/Select.tsx
+++ src/app/_components/Select.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithoutRef } from "react";
 export const Select = (props: ComponentPropsWithoutRef<"select">) => {
   return (
     <select {...props}>
-      {["donuts", "muffin", "tart", "rice", "error"].map((item) => (
+      {["donut", "muffin", "tart", "rice", "error"].map((item) => (
         <option value={item} key={item}>
           {item}
         </option>
